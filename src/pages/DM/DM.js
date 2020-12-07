e0e5bf1d62fd7f3db5aa23622beda053f2ec653e
Eric Park <epark0995@gmail.com>
2020-12-06 18:52:57 -0800

    updating some decks

--- src/pages/DM/DM.js
+++ src/pages/DM/DM.js
@@ -2,7 +2,9 @@ import React from 'react'
 import Card from '../../components/Card'
 
 let characters = [
-    {name: 'Yami', deck:'Dark Magician/Magician Girl'}] 
+    {name: 'Yami', deck:'Dark Magician/Magician Girl'},
+    {name: 'Seto', deck:'N/A'}]
+   
     // 'Seto', 'Joey', 'Mai', 'Tea', 'Yugi', 'Weevil', 'Rex', 'Mako', 'Marik', 'Bakura', 'Bandit Keith', 'Ishizu', 'Odion', 'Pegasus', 'Mokuba', 'Paradox', 'Arkana', 'Espa Roba', 'Tristan', 'Lumis and Umbra']
 
 
