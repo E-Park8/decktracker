77b22a66e2aa2e36870fc5ee9fb840ec0fec1f32
Eric Park <epark0995@gmail.com>
2020-12-09 14:51:42 -0800

    update

--- src/pages/DM/DM.js
+++ src/pages/DM/DM.js
@@ -3,7 +3,28 @@ import Card from '../../components/Card'
 
 let characters = [
     {name: 'Yami', deck:'Dark Magician/Magician Girl'},
-    {name: 'Seto', deck:'N/A'}]
+    {name: 'Seto', deck:'N/A'},
+    {name: 'Joey', deck:'N/A'},
+    {name: 'Mai', deck:'Harpie'},
+    {name: 'Tea', deck:'N/A'},
+    {name: 'Yugi', deck:'Magnet Warriors'},
+    {name: 'Weevil', deck:'Insect'},
+    {name: 'Rex', deck:'Dino'},
+    {name: 'Mako', deck:'Water'},
+    {name: 'Marik', deck:'Dark World with RA'},
+    {name: 'Bakura', deck:'Basic Fiend Deck'},
+    {name: 'Bandit Keith', deck:'Machines'},
+    {name: 'Ishizu', deck:'Dark World'},
+    {name: 'Odion', deck:'N/A'},
+    {name: 'Pegasus', deck:'Toons'},
+    {name: 'Mokuba', deck:'N/A'},
+    {name: 'Paradox', deck:'Gate Guardian'},
+    {name: 'Arkana', deck:'Dark Magician/Magician Girl'},
+    {name: 'Espa Roba', deck:'Jinzo'},
+    {name: 'Tristan', deck:'N/A'},
+    {name: 'Lumis & Umbra', deck:'Masks'},
+]
+
    
     // 'Seto', 'Joey', 'Mai', 'Tea', 'Yugi', 'Weevil', 'Rex', 'Mako', 'Marik', 'Bakura', 'Bandit Keith', 'Ishizu', 'Odion', 'Pegasus', 'Mokuba', 'Paradox', 'Arkana', 'Espa Roba', 'Tristan', 'Lumis and Umbra']
 
--- src/pages/GX/GX.js
+++ src/pages/GX/GX.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Card from '../../components/Card'
 
-const GX = () => {
+let characters = [
+    {name: 'Jaden', deck:'Elemental Heroes, Neos'},
+    {name: 'Aster', deck:'Destiny Heroes'}]
+   
+    // 'Seto', 'Joey', 'Mai', 'Tea', 'Yugi', 'Weevil', 'Rex', 'Mako', 'Marik', 'Bakura', 'Bandit Keith', 'Ishizu', 'Odion', 'Pegasus', 'Mokuba', 'Paradox', 'Arkana', 'Espa Roba', 'Tristan', 'Lumis and Umbra']
+
+
+
+
+const GX = () => { 
     return (
         <> 
             <h1>GX</h1>
+            {/* <ul>
+                <li>{characters[0]}- Dark Magicians</li>
+            </ul> */}
+           
+            {characters.map(characters => <Card characters = {characters}/>)}
+
         </>
     )
 }
